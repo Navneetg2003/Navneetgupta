cba456008701e3350d8e79f8972fdbad0f011c45
Navneet <navneetg1302@gmail.com>
2024-11-16 15:58:13 +0530

    edits

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                 <div className="flex items-center gap-3">
                     <figure className="img-box w-9 h-9 rounded-lg">
                         <img 
-                          src="src\assets\img.jpg" 
+                          src="https://drive.google.com/file/d/1EBhpFxYZY8Qbgcq7qIDR6sB-JUpTtoNr/view?usp=drive_link" 
                           width={40}
                           height={40}
                           alt="Navneet Gupta Potrait"
